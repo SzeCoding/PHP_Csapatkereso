3d7c9de5f66dfb7dcfa5ffd417ba7adf6fb0f195
Tulib17 <tuli.barni.peter@gmail.com>
2023-05-30 15:26:59 +0200

    made logout function

--- PHP_sze/index.php
+++ PHP_sze/index.php
@@ -17,7 +17,7 @@
                 <?php
                     if (isset($_SESSION["username"])){
                         echo "<li><a href='profile.php'>Profile Page</a></li>";
-                        echo "<li><a href='logout.php'>Log out</a></li>";
+                        echo "<li><a href='includes/logout.inc.php'>Log out</a></li>";
                     }else{
                         echo "<li><a href='signup.php'>Sign in</a></li>";
                         echo "<li><a href='login.php'>Log in</a></li>";
