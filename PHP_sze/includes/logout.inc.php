a5f51ed16e44daa55c75f2e70ec440adb98d37b8
Tulib17 <tuli.barni.peter@gmail.com>
2023-06-03 11:20:10 +0200

    cleaned includes folder

--- PHP_sze/includes/logout.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
- header('Access-Control-Allow-Origin: http://localhost:3000');
-    session_start();
-    session_unset();
-    session_destroy();
-    
-    header("location: ../index.php");
