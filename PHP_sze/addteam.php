a37a3bfd8728ecf297c07d8695327de4492c985b
tulibarni <tuli.barni.peter@gmail.com>
2023-06-05 23:38:56 +0200

    added addteam function

--- PHP_sze/addteam.php
+++ PHP_sze/addteam.php
@@ -2,10 +2,16 @@
     header('Access-Control-Allow-Origin: http://localhost:3000');
     header("Access-Control-Allow-Headers: *");
 
+    include 'classes/addteam.classes.php';
+
     $method = $_SERVER['REQUEST_METHOD'];
     switch($method){
         case "POST":
-            $team = json_decode(file_get_contents('php://input'), true);
-            $teamname = $team['teamname'];
-            $teamlimit = $team['teamlimit'];
+            $teamdata = json_decode(file_get_contents('php://input'), true);
+            $teamname = $team['teamdata']['teamname'];
+            $teamlimit = $team['teamdata']['teamlimit'];
+
+            $team = new team;
+            $team->AddTeam($teamname, $teamlimit);
+
     }
--- PHP_sze/classes/addteam.classes.php
+++ PHP_sze/classes/addteam.classes.php
@@ -1,10 +1,9 @@
 <?php
     class team extends Dbh{
 
-        protected function AddTeam($teamname, $teamlimit){
+        public function AddTeam($teamname, $teamlimit){
 
             $stmt = $this->connect()->prepare('INSERT INTO team (teamName, teamLimit)  VALUES(?,?);');
-            $stmt->execute();
 
             if(!$stmt->execute(array($teamname, $teamlimit))){
                 $stmt = null;
