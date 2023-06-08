7d0645e5706d76add9d9c790622b971edf066310
tulibarni <tuli.barni.peter@gmail.com>
2023-06-08 12:09:54 +0200

    made delteam functions in team class

--- PHP_sze/addteam.php
+++ PHP_sze/addteam.php
@@ -3,20 +3,20 @@
     header("Access-Control-Allow-Headers: *");
 
     error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    ini_set('display_errors', 1);
 
     include "classes/dbh.classes.php"; 
     include "classes/team.classes.php";
-    include "classes/team-contr.classes.php";
+    include "classes/addteam-contr.classes.php";
 
     $method = $_SERVER['REQUEST_METHOD'];
     switch($method){
         case "POST":
             $teamData = json_decode(file_get_contents('php://input'), true);
-            $teamName = $teamData['teamdata']['teamname'];
-            $teamLimit = $teamData['teamdata']['teamlimit'];
-            $teamCourse = $teamData['teamdata']['teamcourse'];
-            $teamAdmin = $teamData['teamdata']['teamadmin'];
+            $teamName = $teamData['teamdata']['teamName'];
+            $teamLimit = $teamData['teamdata']['teamLimit'];
+            $teamCourse = $teamData['teamdata']['teamCourse'];
+            $teamAdmin = $teamData['teamdata']['teamAdmin'];
 
             
             $team = new TeamContr($teamCourse, $teamName, $teamLimit, $teamAdmin);
